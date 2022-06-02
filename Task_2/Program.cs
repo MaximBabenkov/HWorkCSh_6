d22b59834e1954665d6693f552b51c68b5ef605d
MaximBabenkov <lostbass@mail.ru>
2022-06-02 20:33:29 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать двумерный массив следующим правилом: Amn=m+n
+Console.Clear();
+Console.Write("Enter your amount of matrix rows: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter your amount of matrix columns: ");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+int[,] a = new int[rows,columns];
+for (int i=0;i<rows;i++)
+{
+    for (int j=0;j<columns;j++)
+    {
+        a[i, j] = i+j; 
+        Console.Write($"{a[i,j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
